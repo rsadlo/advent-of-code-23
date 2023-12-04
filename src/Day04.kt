ecb05cb54d08f4eef9bb078553aa63674a5ec7e6
Robin Sadlo <robin.sadlo@codeagle.de>
2023-12-04 10:24:54 +0100

    day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,17 +1,52 @@
+import java.lang.Integer.min
+import kotlin.system.measureTimeMillis
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val result = input
+            .asSequence()
+            .map { it.split(": ").component2().replace("  ", " ") }
+            .map { it.split(" | ").let { Pair(it.component1().split(" "), it.component2().split(" ")) } }
+            .map { it.first intersect it.second }
+            .filter { it.isNotEmpty() }
+            .map { Math.pow(2.0, it.size - 1.0) }
+            .toList()
+
+        return result.sum().toInt()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        val sc = Array(input.size) { 1 } // start with 1 of each
+        input
+            .asSequence()
+            .map { it.split(": ").component2().replace("  ", " ") }
+            .map { it.split(" | ").let { Pair(it.component1().split(" "), it.component2().split(" ")) } }
+            .map { Pair(it.first.filter { it.isNotEmpty() }, it.second.filter { it.isNotEmpty() }) }
+            .map { it.first intersect it.second }
+            .map { it.count() }
+            .mapIndexed { index, winCount ->
+                val amount = sc[index]
+                IntRange(index + 1, min(index + winCount, input.size)).forEach {
+                    sc[it] = sc[it] + amount
+                }
+            }
+            .toList()
+        return sc.sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day04_test")
+    check(part2(testInput) == 30)
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val input = readInput("Day04")
+    measureTimeMillis {
+        part1(input).println()
+    }.let { println("Took ${it} ms") }
+
+    measureTimeMillis {
+        part2(input).println()
+    }.let { println("Took ${it} ms") }
+
 }
